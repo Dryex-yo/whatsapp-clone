2308b6f6a8647771d8d61e76e53aa229367ba01c
Dryex <derysupriyadi1@gmail.com>
2026-04-17 13:24:16 +0700

    Fix Ziggy Route Error & Clean Layout

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -27,12 +27,7 @@ export default function Authenticated({
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink
-                                    href={route('dashboard')}
-                                    active={route().current('dashboard')}
-                                >
-                                    Dashboard
-                                </NavLink>
+                                {/* Navigation items removed for chat application */}
                             </div>
                         </div>
 
@@ -131,12 +126,7 @@ export default function Authenticated({
                     }
                 >
                     <div className="space-y-1 pb-3 pt-2">
-                        <ResponsiveNavLink
-                            href={route('dashboard')}
-                            active={route().current('dashboard')}
-                        >
-                            Dashboard
-                        </ResponsiveNavLink>
+                        {/* Navigation items removed for chat application */}
                     </div>
 
                     <div className="border-t border-gray-200 pb-1 pt-4">
